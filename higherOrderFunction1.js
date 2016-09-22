2ef611b7be99bd8c835dc51e7c99d7532639a6e4
Amna Zletni <azletni13@gmail.com>
2016-09-22 18:40:15 +0000

    adds first exercise

---
+++ higherOrderFunction1.js
@@ -0,0 +1,11 @@
+
+function map(array, cb) {
+  var newArr = [];
+
+  array.forEach(function(ele) {
+    newArr.push(cb(ele));
+  })
+  return newArr;
+}
+
+console.log(map(["ground", "control", "to", "major", "tom"], (elm) => { return elm.length }));
